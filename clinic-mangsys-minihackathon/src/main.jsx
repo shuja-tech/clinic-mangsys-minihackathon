9d261e03a5b1ba00fe0f7998f3172b3c4b05a17a
m78370002-spec <m78370002@gmail.com>
2026-03-04 01:29:03 +0500

    Fix: Login button navigation - added hashchange listener for proper routing

--- clinic-mangsys-minihackathon/src/main.jsx
+++ clinic-mangsys-minihackathon/src/main.jsx
@@ -1,12 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useState, useEffect } from 'react'
 import './index.css'
 import Home from './Home.jsx'
 import LoginSignup from './LoginSignup.jsx'
 
 // Simple routing based on URL hash
 const App = () => {
-  const hash = window.location.hash;
+  const [hash, setHash] = useState(window.location.hash);
+  
+  useEffect(() => {
+    const handleHashChange = () => {
+      setHash(window.location.hash);
+    };
+    
+    window.addEventListener('hashchange', handleHashChange);
+    return () => window.removeEventListener('hashchange', handleHashChange);
+  }, []);
   
   if (hash === '#login' || hash === '#signup') {
     return <LoginSignup />;
